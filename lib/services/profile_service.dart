37a8b0510533a3a7b112d3bfead9a648fd519cb6
alfygitau <alfygitau@gmail.com>
2026-04-21 18:38:23 +0300

    changes

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -17,4 +17,85 @@ class ProfileService {
       return (null, ["Authentication Error!", "An unknown error occurred."]);
     }
   }
+
+  Future<(Response?, List<String>?)> updateAddress({
+    required String id,
+    required String county,
+    required String physicalAddress,
+    required String subcounty,
+  }) async {
+    try {
+      final response = await _secureClient.patch(
+        '/address/$id',
+        data: {
+          "physical_address": physicalAddress,
+          "county": county,
+          "subcounty": subcounty,
+        },
+      );
+      return (response, null);
+    } on DioException catch (e) {
+      final apiException = ApiException();
+      final errorMessages = apiException.getExceptionMessage(e);
+      return (null, errorMessages);
+    } catch (e) {
+      return (null, ["Authentication Error!", "An unknown error occurred."]);
+    }
+  }
+
+  Future<(Response?, List<String>?)> updateFinancials({
+    required String id,
+    required String employmentType,
+    required String kra,
+    required String jobTitle,
+    required String income,
+  }) async {
+    try {
+      final response = await _secureClient.patch(
+        '/customer/$id',
+        data: {
+          "employment_type": employmentType,
+          "kraPin": kra,
+          "occupation": jobTitle,
+          "income_range": income,
+        },
+      );
+      return (response, null);
+    } on DioException catch (e) {
+      final apiException = ApiException();
+      final errorMessages = apiException.getExceptionMessage(e);
+      return (null, errorMessages);
+    } catch (e) {
+      return (null, ["Authentication Error!", "An unknown error occurred."]);
+    }
+  }
+
+  Future<(Response?, List<String>?)> updateKin({
+    required String id,
+    required String fullName,
+    required String birthDate,
+    required String relationship,
+    required String phone,
+    required String location,
+  }) async {
+    try {
+      final response = await _secureClient.patch(
+        '/customer/next-of-kin/$id',
+        data: {
+          "name": fullName,
+          "dateOfBirth": birthDate,
+          "relationship": relationship,
+          "phoneNumber": phone,
+          "location": location,
+        },
+      );
+      return (response, null);
+    } on DioException catch (e) {
+      final apiException = ApiException();
+      final errorMessages = apiException.getExceptionMessage(e);
+      return (null, errorMessages);
+    } catch (e) {
+      return (null, ["Authentication Error!", "An unknown error occurred."]);
+    }
+  }
 }
