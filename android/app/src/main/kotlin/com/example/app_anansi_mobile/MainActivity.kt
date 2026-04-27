91c2857dcb51b07f202212d474084b230d70a547
alfygitau <alfygitau@gmail.com>
2026-04-27 15:49:11 +0300

    changes

--- android/app/src/main/kotlin/com/example/app_anansi_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/app_anansi_mobile/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.app_anansi_mobile
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity // Check this line carefully
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterFragmentActivity()
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
